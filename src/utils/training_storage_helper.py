64e81724e1f9f40721b587d8b4c61a29538b1005
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-10-10 21:12:56 +0700

    feat/pbl6-find-best-parameters

--- src/utils/training_storage_helper.py
+++ src/utils/training_storage_helper.py
@@ -1,10 +1,17 @@
 import json
 import pickle
+import os
 
-def save_params(params, file_path: str):
+def save_params(params: dict, file_path: str):
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
     with open(file_path, 'w') as f:
         json.dump(params, f)
 
-def save_model(model, file_name):
+def save_model(model, file_name: str):
+    if not os.path.exists(os.path.dirname(file_name)):
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+
     with open(file_name, "wb") as file:
         pickle.dump(model, file)
